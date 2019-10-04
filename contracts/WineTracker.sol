00e2d92433ac34f3c0555356a8c34cfd929a368f
Tobie Praz <tobie.praz@hevs.ch>
2019-10-05 00:33:32 +0200

    Contract refactor

--- contracts/WineTracker.sol
+++ contracts/WineTracker.sol
@@ -1,12 +1,59 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.5.6;
 
 contract WineTracker {
+
+    // Wine producer representation
+    struct Producer {
+        // Address of producer account
+        // Only this account may emit shipment traces on this producer's behalf
+        address account;
+
+        // IPFS hash for extended info, stored off-chain for costs
+        string extendedInfo;
+
+        // Balance of allowed wine production left
+        // Unit : centiliters (1/100th of a liter)
+        uint32 clBalance;
+    }
+
+    // Tracing certificate representation
+    struct ShipmentTrace {
+
+        uint32 producerId;
+
+        // Container capacity in 1/100th of a liter
+        // Example : standard wine bottle has a capacity of 75
+        uint16 clCapacity;
+
+        // Production date (timestamp)
+        uint productionDate;
+
+        // IPFS hash for extended info of this certificate.
+        // Data includes eventual traceability event log
+        // Off-chain storage
+        string extendedInfo;
+
+        // Validity flag
+        // True by default. Set to false in case the shipment compliance with the authority
+        // requirement is compromised. This prevents the winemaker from compromising quality
+        // even after being awarded the production allowance
+        bool isValid;
+    }
 
     // Contract owner address
     address owner;
 
     // Production fee for producing a wine shipment unit
-    uint productionFee = 0.0001 ether;
+    uint constant productionFee = 0.0001 ether;
+
+    // The producers storage
+    Producer[] producers;
+
+    // The shipment traces storage
+    ShipmentTrace[] shipmentTraces;
+
+    uint producerCount = 0;
+    uint tracesCount = 0;
 
     // This modifier restricts a function usage to be only accessible by
     // this contract owner, in the current case the quota authority agency.
@@ -17,95 +64,37 @@ contract WineTracker {
 
     // This modifier restricts a function usage to be only accessible by
     // the associated wine producer.
-    modifier isProducer(uint _producerId) {
-        require(msg.sender == producers[_producerId].producerAddress);
+    modifier isProducer(uint _id) {
+        require(msg.sender == producers[_id].account);
         _;
     }
 
-    // Wine producer representation
-    struct WineProducer {
-        // IPFS hash for extended info, stored off-chain for costs
-        string extendedInfo;
-
-        // Balance of allowed wine production left
-        // Unit : centiliters (1/100th of a liter)
-        uint32 clBalance;
-
-        // Address of producer account
-        // Only this account may emit shipment traces on this producer's behalf
-        address producerAddress;
-    }
-
-    // Tracing certificate representation
-    struct WineShipmentTrace {
-
-        // Producer ID reference
-        uint producerId;
-
-        // IPFS hash for extended info of this certificate.
-        // Data includes eventual traceability event log
-        // Off-chain storage
-        string extendedInfo;
-
-        // Container capacity in 1/100th of a liter
-        // Example : standard wine bottle has a capacity of 75
-        uint16 clCapacity;
-
-        // Validity flag
-        // True by default. Set to false in case the shipment compliance with the authority
-        // requirement is compromised. This prevents the winemaker from compromising quality
-        // even after being awarded the production allowance
-        bool is_valid
-
-        // Production date (timestamp)
-        uint productionDate;
-
-    }
-
-    // The producers storage
-    WineProducer[] producers;
-
-    // The shipment traces storage
-    WineShipmentTrace[] shipmentTraces;
-
-    uint producerCount = 0;
-    uint tracesCount = 0;
-
     // Register new producer. Only runnable by the authority.
-    function registerProducer(address _producerAddress, string _producerInfoHash) onlyOwner returns(uint) {
-        producerCount++;
-        producers.push(WineProducer (_producerInfoHash, 0, _producerAddress);
-        return (producerCount - 1);
-    }
-
-    // Increase producer wine output allowance by the specified value in 1/100th of liter
-    function increaseProducerQuota(uint _producerId, uint32 _clQuota) onlyOwner {
-        producers[_producerId].clBalance += _clQuota;
+    function registerProducer(address _address, string memory _infoHash) onlyOwner public returns (uint) {
+        producers.push(Producer(_address, _infoHash, 0));
+        return producerCount++;
     }
 
     // Set producer wine output allowance by the specified value in 1/100th of liter
-    function setProducerQuota(uint _producerId, uint32 _clQuota) onlyOwner {
-        producers[_producerId].clBalance = _clQuota;
+    function setProducerQuota(uint _id, uint32 _clQuota) onlyOwner public {
+        producers[_id].clBalance = _clQuota;
     }
 
     // Action from the producer to emit a wine shipment
     // Payable with the appropriate fee
-    function emitWineShipment(uint _producerId, uint16 _clContainerCapacity, string _extendedInfoHash) isProducer(_producerId) payable {
+    function emitWineShipment(uint32 _producerId, uint16 _clContainerCapacity, string memory _extendedInfoHash) isProducer(_producerId) payable public {
         require(msg.value == productionFee);
         require(producers[_producerId].clBalance >= _clContainerCapacity);
         // Withdraw capacity from total allowance
         producers[_producerId].clBalance -= _clContainerCapacity;
         // Emit certificate
-        shipmentTraces.push(WineShipmentTrace(_producerId, _extendedInfo, _clContainerCapacity, true, now));
+        shipmentTraces.push(ShipmentTrace(_producerId, _clContainerCapacity, now, _extendedInfoHash, true));
     }
 
     // Action available for everyone to retrieve traceability info from a wine shipment
     // Useful for customer verification
-    function getWineShipmentInfo(uint _shipmentId) external view returns (string, string, uint16, bool, uint) {
-        WineShipmentTrace trace = shipmentTraces[_shipmentId];
-
+    function getWineShipmentInfo(uint _shipmentId) external view returns (string memory, string memory, uint16, bool, uint) {
+        ShipmentTrace memory trace = shipmentTraces[_shipmentId];
         return (trace.extendedInfo, trace.extendedInfo, trace.clCapacity, trace.isValid, trace.productionDate);
     }
-
-
 }
